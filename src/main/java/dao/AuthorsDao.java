feda96c2bebbcca13d17096005f44770336497f8
John Buhr <Johnbuhr92@gmail.com>
2023-05-15 12:00:25 -0500

    final commit for lecture

---
+++ src/main/java/dao/AuthorsDao.java
@@ -0,0 +1,54 @@
+package dao;
+
+import com.mysql.cj.jdbc.Driver;
+import config.Config;
+import models.Ad;
+import models.Author;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthorsDao implements Authors{
+
+    private Connection connection;
+
+    public AuthorsDao(){
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    Config.getUrl(),
+                    Config.getUser(),
+                    Config.getPassword()
+            );
+        } catch (SQLException sqlx){
+            throw new RuntimeException("Error connecting to the databse", sqlx);
+        }
+
+    }
+
+    @Override
+    public List<Author> all() {
+        List<Author> authors = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            // execute statement
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM bigfoot_test_db.authors");
+            while (resultSet.next()) {
+                Author author = new Author(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name")
+                );
+                authors.add(author);
+            }
+            } catch (SQLException sqlx){
+                throw new RuntimeException("error connecting to database", sqlx);
+            }
+        return authors;
+        }
+
+    @Override
+    public void insert(Author author) {
+
+    }
+}
